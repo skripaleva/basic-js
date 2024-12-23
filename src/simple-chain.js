82588c0433b7adc493121ff8edaea6c875c3d723
Evgeniya <evgeniya_ignaten@mail.ru>
2024-12-23 22:39:15 +0300

    feat: add solution for simple-chain (44 passing)

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!Number.isInteger(position)) {
+      this.arr = [];
+      throw new NotImplementedError('You can\'t remove incorrect link!');
+    }
+    this.arr.splice(position - 1, 1);
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.arr.join('~~');
+    this.arr =[];
+    return res
   }
 };
 
