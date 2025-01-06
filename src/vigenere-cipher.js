ff69732b86976015cc898878cb2330a6a5f7f556
Evgeniya <evgeniya_ignaten@mail.ru>
2025-01-06 20:18:05 +0300

    feat: add solution for vigenere-cipher.js (61 passing)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,86 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(val = true) {
+    this.val = val;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.numAlph = {};
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getObject() {
+    for (let i = 0; i < this.alphabet.length; i++) {
+      this.numAlph[this.alphabet[i]] = i;
+    }
+  }
+  encrypt(text, key) {
+    console.log(this.reverse)
+    if (arguments.length < 2) throw new NotImplementedError('Incorrect arguments!');
+    this.getObject()
+    let res = ''
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (!this.alphabet.includes(text[i].toUpperCase())) {
+
+        res += text[i]
+      } else {
+        res += this.alphabet[(this.numAlph[text.toUpperCase()[i]] + this.numAlph[key.toUpperCase()[count % key.toUpperCase().length]]) % this.alphabet.length];
+        count +=1;
+      }
+    }
+
+    if(!this.val) {
+      res = res.split('').reverse().join('')
+    }
+
+    return res;
+  }
+  decrypt(text, key) {
+    if (arguments.length < 2) throw new NotImplementedError('Incorrect arguments!');
+    this.getObject()
+    let res = ''
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (!this.alphabet.includes(text[i].toUpperCase())) {
+
+        res += text[i]
+      } else {
+        res += this.alphabet[(this.numAlph[text.toUpperCase()[i]] - this.numAlph[key.toUpperCase()[count % key.toUpperCase().length]] + this.alphabet.length) % this.alphabet.length];
+        count +=1;
+      }
+    }
+    if(!this.val) {
+      res = res.split('').reverse().join('')
+    }
+    return res;
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+const directMachine = new VigenereCipheringMachine();
+// console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse'))
+
+const reverseMachine = new VigenereCipheringMachine(true);
+reverseMachine.encrypt('attack at dawn!', 'alphonse')
+console.log(reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') )
+
+
+
